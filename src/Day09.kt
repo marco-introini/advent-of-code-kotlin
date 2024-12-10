ab7c21fb74c542094f2ad90a429139e09e6e5d81
Marco "Mint" Introini <mint.dev@pm.me>
2024-12-10 17:37:46 +0100

    Day 9 part2 wip

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,3 +1,5 @@
+import kotlin.collections.containsKey
+
 data class DiskPoint(val fileId: Int)
 
 data class Disk(
@@ -30,7 +32,7 @@ data class Disk(
                 if (hasNoHoles()) {
                     return
                 }
-                for (j in 0 until size) {
+                for (j in 0 until i) {
                     if (!map.containsKey(j)) {
                         val tempPoint = map[i]
                         if (tempPoint != null) {
@@ -44,6 +46,49 @@ data class Disk(
             }
         }
     }
+
+    fun Disk.findFirstEmptySpace(n: Int): Int? {
+        var consecutiveEmpty = 0
+        var startIndex = -1
+
+        for (i in 0 until size) {
+            if (!map.containsKey(i)) {
+                if (consecutiveEmpty == 0) {
+                    startIndex = i
+                }
+                consecutiveEmpty++
+                if (consecutiveEmpty >= n) {
+                    return startIndex
+                }
+            } else {
+                consecutiveEmpty = 0
+                startIndex = -1
+            }
+        }
+
+        // Nessuno spazio trovato
+        return null
+    }
+
+    fun reArrangeByLength() {
+        // Ordina gli entry della mappa in base al fileId in ordine decrescente
+        val sortedEntries = map.values.toSet()
+
+        for (entry in sortedEntries) {
+            val fileSize = map.count { it.value.fileId == entry.fileId }
+
+            val firstSpace = findFirstEmptySpace(fileSize)
+
+            if (firstSpace != null) {
+                val tempPoint = entry
+                for (i in firstSpace until firstSpace + fileSize) {
+                    map.filter { it.value.fileId == tempPoint.fileId }.forEach { map.remove(it.key) }
+                    map[i] = tempPoint
+                }
+            }
+        }
+    }
+
 }
 
 
@@ -83,15 +128,27 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        return 0L
+        val disk = generateDisk(input[0])
+        disk.reArrangeByLength()
+
+        var result = 0L
+        disk.map.forEach() { index, it ->
+            result += index * it.fileId.toLong()
+        }
+
+        return result
     }
 
     val testInput = readInput("Day09_test")
     val disk = generateDisk(testInput[0])
+    println("TEST PART1")
     println(disk.toString())
     check(disk.toString() == "00...111...2...333.44.5555.6666.777.888899")
     disk.reArrange()
-    println(disk)
+    println("TEST PART2")
+    val disk2 = generateDisk(testInput[0])
+    disk2.reArrangeByLength()
+    println(disk2.toString())
 
     check(part1(testInput) == 1928L)
     check(part2(testInput) == 2858L)
